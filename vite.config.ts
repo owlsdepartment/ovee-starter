91680fd39c38854c88a1d97b72e710131f8fce99
Miłosz Mandowski <milosz.m20@gmail.com>
2024-11-18 11:09:58 +0100

    feat: update Ovee version and JSX support

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-	esbuild: {
-		jsxDev: false
-	},
     resolve: {
 		alias: {
 			// pre-configured aliases, change them freely!
